0014959eb0a07012137025711294d5ed8ae52913
kousuke1201abe <k-abe@giftee.co>
2020-01-01 13:17:03 +0900

    refix db/seed/development/events.rb

--- db/seed/development/events.rb
+++ db/seed/development/events.rb
@@ -1,5 +1,7 @@
 Nightclub.first.tap do |nightclub|
-  nightclub.find_or_create_by!(name: "test_club") do |event|
+  nightclub.events.find_or_create_by!(name: "test_club") do |event|
     event.tag = "#test_tag"
+    event.start_at = Time.zone.now
+    event.end_at = Time.zone.now.since(1.day)
   end
 end
