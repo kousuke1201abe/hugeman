837300d00e4712ffb78241b95c864b9701d2fe45
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-01-03 22:14:59 +0900

    organizer schemata

--- schemata/event/organizers.rb
+++ schemata/event/organizers.rb
@@ -2,8 +2,8 @@ create_table :event_organizers do |t|
   t.string :name,
            null: false,
            index: {
-             unique: true,
-             name: :idx_organizers_1,
+             name: :idx_event_organizers_1,
+             
            }
 end
 
