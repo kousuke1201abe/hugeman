3aee43ae55fba46d4e8a6bb8fcdb602112158968
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-01-04 18:15:34 +0900

    seed修正

--- db/seed/development/event/organizers.rb
+++ db/seed/development/event/organizers.rb
@@ -1,3 +1,3 @@
-Event::Organizer.create!(name: "test organizer").tap do |event|
+Event:.find_by!(name: "test_organizer").tap do |event|
   event.organizers.find_or_create_by!(url: "test organizer")
 end
