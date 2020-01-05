b4a8f43402a87cdb82669015d6145a41cf3badf0
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-01-05 18:17:02 +0900

    has_many修正

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-  has_many :artists, through: :artist_identifyings
+  has_many :events, through: :artist_identifyings
   has_many :event_artist_identifyings,
             class_name: "Event::ArtistIdentifying",
             foreign_key: :event_artist_identifying_id
