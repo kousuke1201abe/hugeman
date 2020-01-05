f93416f62a3e231c1ac531426c3dd00ca4034bd9
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-01-05 16:08:36 +0900

    factory修正

--- spec/factories/event_artistidentifyings.rb
+++ spec/factories/event_artistidentifyings.rb
@@ -1,7 +1,6 @@
 FactoryBot.define do
   factory :event_artistidentifying, class: Event::Artistidentifying do
     event
-    sequence(:event_id){ |n| "#{n}" }
-    sequence(:artist_id){ |n| "#{n}" }
+    artist
   end
 end
