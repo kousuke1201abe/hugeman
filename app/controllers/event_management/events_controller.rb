ee4ba2ce1140dd0a4814b167a07b89c79c46183b
bonobozzzz <kawaiiyumegirl@gmail.co>
2020-01-14 02:47:29 +0900

    add index action to event_controller.rb

--- app/controllers/event_management/events_controller.rb
+++ app/controllers/event_management/events_controller.rb
@@ -1,9 +1,9 @@
 class EventManagement::EventsController < EventManagement::ApplicationController
   def index
+    @event_management_events = EventManagement::Event.filter_user_by(@current_user.id)
   end
 
   def show
     @event_management_event = EventManagement::Event.find(params[:id])
   end
-
 end
