9b7b12cf978fa408e0bc3daf9b839385fb1b7ec1
bonobozzzz <kawaiiyumegirl@gmail.co>
2020-01-14 02:48:43 +0900

    add method ".all" to app/models/event_management/event.rb

--- app/models/event_management/event.rb
+++ app/models/event_management/event.rb
@@ -1,6 +1,10 @@
 class EventManagement::Event < ApplicationModel
   attribute :event, default: -> { Event.new }
-  
+
+  def self.all
+    new(event: Event.all)
+  end
+
   def self.find(id)
     new(event: Event.find(id))
   end
