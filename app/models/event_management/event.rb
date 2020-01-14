76b9bcfa14c75f8e270f504292fee8e90f3b8f36
bonobozzzz <kawaiiyumegirl@gmail.co>
2020-01-15 01:59:47 +0900

    add class_method where

--- app/models/event_management/event.rb
+++ app/models/event_management/event.rb
@@ -1,6 +1,11 @@
 class EventManagement::Event < ApplicationModel
   attribute :event, default: -> { Event.new }
-  
+
+  def self.filter_user_by(args)
+    user = User.find(args)
+    user.events
+  end
+
   def self.find(id)
     new(event: Event.find(id))
   end
