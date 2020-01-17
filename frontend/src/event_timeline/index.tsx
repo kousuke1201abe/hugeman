2d795997f65cdc74a4b9bde55fc4dba98b41c83e
kousuke1201abe <k-abe@giftee.co>
2020-01-17 21:23:09 +0900

    material ui を導入

--- frontend/src/event_timeline/index.tsx
+++ frontend/src/event_timeline/index.tsx
@@ -3,12 +3,16 @@ import * as reactDom from 'react-dom';
 import { ApolloProvider } from 'react-apollo';
 import { client } from '../../common/api/graphql/apolloClient';
 import { Hello } from '../event_timeline/components/hello';
+import Button from '@material-ui/core/Button';
 
 class App extends React.Component<any, any> {
   render () {
     return(
       <div>
         <Hello/>
+        <Button variant="contained" color="primary">
+          aaaaaaaaaaaaa
+        </Button>
       </div>
     );
   }
