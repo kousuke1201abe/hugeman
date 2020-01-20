205df2f09dca9702b06ec71f58a7ae3b6e2b6c23
bonobozzzz <kawaiiyumegirl@gmail.co>
2020-01-20 18:46:32 +0900

    EventManagement::Event にdelegateを追加

--- app/models/event_management/event.rb
+++ app/models/event_management/event.rb
@@ -1,6 +1,11 @@
 class EventManagement::Event < ApplicationModel
   attribute :event, default: -> { Event.new }
   delegate :name, to: :event
+  delegate :artists, to: :event
+  delegate :start_at, to: :event
+  delegate :end_at, to: :event
+  delegate :tag, to: :event
+  delegate :nightclub, to: :event
 
   def self.filter_user_by(args)
     user = User.find(args)
