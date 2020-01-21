5c9aca5036c1022cb2250c7969ec3963a494d9ca
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-01-21 20:12:07 +0900

    remove_un-used_actions

--- app/controllers/event_management/artists_controller.rb
+++ app/controllers/event_management/artists_controller.rb
@@ -1,16 +1,6 @@
 class EventManagement::ArtistsController < EventManagement::ApplicationController
-  def index
-  end
-
-  def show
-    @event_management_event = EventManagement::Artists.find(params[:id])
-  end
 
   def new
-  
-  end
-
-  def create
   
   end
 
--- app/controllers/event_management/nightclubs_controller.rb
+++ app/controllers/event_management/nightclubs_controller.rb
@@ -1,16 +1,6 @@
 class EventManagement::NightclubsController < EventManagement::ApplicationController
-  def index
-  end
-
-  def show
-    @event_management_event = EventManagement::Nightclubs.find(params[:id])
-  end
 
   def new
-  
-  end
-
-  def create
   
   end
 
