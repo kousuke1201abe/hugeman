4cbb84ceb88049cd1477deaae4c17a01cc29b0b6
bonobozzzz <kawaiiyumegirl@gmail.co>
2020-01-22 23:24:58 +0900

    updateアクションをコントローラに追加

--- app/controllers/event_management/events_controller.rb
+++ app/controllers/event_management/events_controller.rb
@@ -10,4 +10,21 @@ class EventManagement::EventsController < EventManagement::ApplicationController
   def edit
     @event_management_event = EventManagement::Event.find(params[:id])
   end
+
+  def update
+    @event_management_event = EventManagement::Event.find(params[:id])
+    if @event_management_event.update!(event_management_event_params)
+      redirect_to event_management_events_path(@event_management_event.event), notice: 'イベント情報を更新しました'
+    end
+  end
+
+  private
+  def event_management_event_params
+    params.require(:event).permit(
+      :name,
+      :tag,
+      :start_at,
+      :end_at
+    )
+  end
 end
