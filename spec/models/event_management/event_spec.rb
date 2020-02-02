b7700c1f38628cfca11ba76538bdfc4dbff36459
bonobozzzz <kawaiiyumegirl@gmail.com>
2020-02-02 18:30:40 +0900

    add spec for updating event

--- spec/models/event_management/event_spec.rb
+++ spec/models/event_management/event_spec.rb
@@ -13,4 +13,30 @@ RSpec.describe EventManagement::Event, type: :model do
       expect(User.count).to eq(1)
     end
   end
+
+  describe "#update" do
+    subject { event.update!(args) }
+
+    let!(:event) { create(:event) }
+    let!(:args) do
+      {
+        name: "test name",
+        tag: "test tag",
+        start_at: Time.zone.parse("2018/12/01 00:00:00"),
+        end_at: Time.zone.parse("2018/12/02 00:00:00"),
+      }
+     end
+
+     context "with valid args" do
+      specify do
+        expect(event).to be_valid
+        subject
+        expect(event.name).to eq(args[:name])
+        expect(event.tag).to eq(args[:tag])
+        expect(event.start_at).to eq(args[:start_at])
+        expect(event.end_at).to eq(args[:end_at])
+      end
+     end
 end
+end
+
