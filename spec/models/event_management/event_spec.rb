89bb7b2ee10336ec21c9b40933aefbda3098b8bc
takumi kishimoto <51050558+bonobozzzz@users.noreply.github.com>
2020-02-06 23:15:56 +0900

    Update event_spec.rb

--- spec/models/event_management/event_spec.rb
+++ spec/models/event_management/event_spec.rb
@@ -13,73 +13,73 @@ RSpec.describe EventManagement::Event, type: :model do
        end
       end
 
-      describe "#update!" do
-        subject { event.update!(args) }
+  describe "#update!" do
+    subject { event.update!(args) }
 
-        let!(:event) { create(:event) }
-        let!(:args) do
-          {
-            name: "test name",
-            tag: "test tag",
-            start_at: Time.zone.parse("2018/12/01 00:00:00"),
-            end_at: Time.zone.parse("2018/12/02 00:00:00"),
-          }
-        end
+    let!(:event) { create(:event) }
+    let!(:args) do
+      {
+        name: "test name",
+        tag: "test tag",
+        start_at: Time.zone.parse("2018/12/01 00:00:00"),
+        end_at: Time.zone.parse("2018/12/02 00:00:00"),
+      }
+    end
 
-      context "with valid args" do
-        
-        specify do
-          expect(event).to be_valid
-          subject
-          expect(event.name).to eq(args[:name])
-          expect(event.tag).to eq(args[:tag])
-          expect(event.start_at).to eq(args[:start_at])
-          expect(event.end_at).to eq(args[:end_at])
-        end
+    context "with valid args" do
+
+      specify do
+        expect(event).to be_valid
+        subject
+        expect(event.name).to eq(args[:name])
+        expect(event.tag).to eq(args[:tag])
+        expect(event.start_at).to eq(args[:start_at])
+        expect(event.end_at).to eq(args[:end_at])
+      end
+    end
+
+    context "with blank name" do
+      before do
+        args[:name] = ""
       end
 
-      context "with blank name" do
-        before do
-          args[:name] = ""
-        end
-
-        specify do
-            expect(event).to be_valid
-            expect{ subject }.to raise_error(ActiveRecord::RecordInvalid)
-         end
-      end
-
-      context "with blank tag" do
-        before do
-          args[:tag] = ""
-        end
-
-        specify do
+      specify do
           expect(event).to be_valid
           expect{ subject }.to raise_error(ActiveRecord::RecordInvalid)
-        end
+       end
+    end
+
+    context "with blank tag" do
+      before do
+        args[:tag] = ""
       end
-
-      context "with blank start_at" do
-        before do
-          args[:start_at] = ""
-        end
-
-      specify do
-        expect(event).to be_valid
-        expect{ subject }.to raise_error(ActiveRecord::RecordInvalid)
-        end
-      end
-
-      context "with blank end_at" do
-        before do
-          args[:end_at] = ""
-        end
 
       specify do
         expect(event).to be_valid
         expect{ subject }.to raise_error(ActiveRecord::RecordInvalid)
       end
+    end
+
+    context "with blank start_at" do
+      before do
+        args[:start_at] = ""
+      end
+
+    specify do
+      expect(event).to be_valid
+      expect{ subject }.to raise_error(ActiveRecord::RecordInvalid)
+      end
+    end
+
+    context "with blank end_at" do
+      before do
+        args[:end_at] = ""
+      end
+
+    specify do
+      expect(event).to be_valid
+      expect{ subject }.to raise_error(ActiveRecord::RecordInvalid)
+    end
     end
   end
 end
