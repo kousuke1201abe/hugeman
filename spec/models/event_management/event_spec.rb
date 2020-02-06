6da1d6eaaa5b30be67d94a1a07c7669bb86df9f2
takumi kishimoto <51050558+bonobozzzz@users.noreply.github.com>
2020-02-06 23:08:42 +0900

    Update event_spec.rb

--- spec/models/event_management/event_spec.rb
+++ spec/models/event_management/event_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe EventManagement::Event, type: :model do
   describe ".filter_user_by" do
       subject { EventManagement::Event.filter_user_by(args) }
+    
         let!(:user) { create(:user) }
         let!(:event) { create(:event, user: user) }
         let!(:args) { user.id }
@@ -23,9 +24,10 @@ RSpec.describe EventManagement::Event, type: :model do
             start_at: Time.zone.parse("2018/12/01 00:00:00"),
             end_at: Time.zone.parse("2018/12/02 00:00:00"),
           }
-      end
+        end
 
       context "with valid args" do
+        
         specify do
           expect(event).to be_valid
           subject
@@ -41,10 +43,10 @@ RSpec.describe EventManagement::Event, type: :model do
           args[:name] = ""
         end
 
-      specify do
-          expect(event).to be_valid
-          expect{ subject }.to raise_error(ActiveRecord::RecordInvalid)
-        end
+        specify do
+            expect(event).to be_valid
+            expect{ subject }.to raise_error(ActiveRecord::RecordInvalid)
+         end
       end
 
       context "with blank tag" do
