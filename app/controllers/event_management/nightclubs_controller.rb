24256fdc84b15c157721341008217c22e2765955
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-02-09 19:40:05 +0900

    rescue文追加

--- app/controllers/event_management/nightclubs_controller.rb
+++ app/controllers/event_management/nightclubs_controller.rb
@@ -5,9 +5,10 @@ class EventManagement::NightclubsController < EventManagement::ApplicationContro
   end
 
   def create
-    event_management_nightclub = EventManagement::Nightclub.create!(nightclub_params)
-    if event_management_nightclub == false
-      redirect_to :new_event_management_nightclub_path
+    begin
+      event_management_nightclub = EventManagement::Nightclub.create!(nightclub_params)
+    rescue
+      redirect_to new_event_management_nightclub_path
     end
   end
 
