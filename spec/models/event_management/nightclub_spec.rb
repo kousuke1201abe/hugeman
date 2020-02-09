17e72d162ffc68db0a705b908f6bb10648de885d
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-02-09 19:58:55 +0900

    テストの修正

--- spec/models/event_management/nightclub_spec.rb
+++ spec/models/event_management/nightclub_spec.rb
@@ -27,7 +27,6 @@ RSpec.describe EventManagement::Nightclub, type: :model do
 
       specify do
         expect(Nightclub.count).to eq(0)
-        subject
         expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
         expect(Nightclub.count).to eq(0)
       end
