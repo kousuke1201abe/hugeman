2db66d4d9c6c08f32056cb71d055573d1d6f6141
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-02-27 20:51:37 +0900

    テスト追加

--- spec/requests/event_management/nightclubs_spec.rb
+++ spec/requests/event_management/nightclubs_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe "EventManagement::NightclubsController", type: :request do
   include_context "with user session"
-  
+
   describe "GET new_event_management_nightclub_path" do
     subject { get new_event_management_nightclub_path }
 
@@ -23,13 +23,27 @@ RSpec.describe "EventManagement::NightclubsController", type: :request do
 
   describe "POST event_management/nitghtclubs/:id" do
     let!(:params) do
-      { name: "test club" }
+      { name: "test club", id: 1 }
     end
     context 'with valid params' do
       subject { post event_management_nightclubs_path, params: { nightclub: params } }
       specify {
         expect(subject).to eq 302
+        expect(subject).to render_template(new_event_management_artist_path)
+        
       }
+    end
+    context 'with invalid params' do
+      subject { post event_management_nightclubs_path, params: { nightclub: params } }
+
+        let(:params) do
+          { name: "", id: 1 }
+        end
+
+        it 'render error page' do
+          expect(subject).to eq 400
+          expect(subject).to render_template(:new)
+        end
     end
   end
 end
