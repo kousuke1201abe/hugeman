47511930365db96c9031024e1377547650423af5
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-04-05 20:39:02 +0900

    Artistのテスト

---
+++ spec/models/event_management/artist_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.describe EventManagement::Artist, type: :model do
+  describe ".create!" do
+    subject { EventManagement::Artist.create!(args) }
+
+    let!(:args) do
+      {
+        name: "test artist"
+      }
+    end
+
+    context "with valid args" do
+
+      specify do
+        expect(Artist.count).to eq(0)
+        subject
+        expect(Artist.count).to eq(1)
+        expect(Artist.first.name).to eq(args[:name])
+      end
+    end
+
+    context "with blank name" do
+      before do
+        args[:name] = ""
+      end
+
+      specify do
+        expect(Artist.count).to eq(0)
+        expect { subject }.to raise_error(ActiveRecord::RecordInvalid)
+        expect(Artist.count).to eq(0)
+      end
+    end
+  end
+end
