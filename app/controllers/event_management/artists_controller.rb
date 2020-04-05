f94ee312c8fe147165fef0aabc69345881e749e0
akkt222 <55902529+akkt222@users.noreply.github.com>
2020-04-05 20:36:22 +0900

    Artistコントローラ

--- app/controllers/event_management/artists_controller.rb
+++ app/controllers/event_management/artists_controller.rb
@@ -1,7 +1,20 @@
 class EventManagement::ArtistsController < EventManagement::ApplicationController
 
   def new
-  
+  end
+
+  def create
+      event_management_artist = EventManagement::Artist.create!(artist_params)
+      redirect_to event_management_events_path, notice: "出演者が登録されました"
+    rescue ActiveRecord::RecordInvalid => e
+      render :new, status: 400
+  end
+
+
+  private
+
+  def nightclub_params
+    params.require(:nightclub).permit(:name)
   end
 
 end
